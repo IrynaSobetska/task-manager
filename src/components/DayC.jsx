d571b9ba2ad97654b894e591854c5fb786f82a00
IrynaSobetska <sobetska.ira@gmail.com>
2026-04-01 16:47:04 +0200

    add update event function

--- src/components/DayC.jsx
+++ src/components/DayC.jsx
@@ -11,7 +11,6 @@ const DayC = ({ day, setIsDay, setEvents, events }) => {
   const [description, setDescription] = useState("");
 
   let todayEvents = events.filter((event) => event.date == day[4]);
-  // console.log(todayEvents);
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
@@ -33,7 +32,7 @@ const DayC = ({ day, setIsDay, setEvents, events }) => {
     event.preventDefault();
 
     const newEvent = {
-      id: Math.random() * 10,
+      id: events[events.length - 1].id + 1,
       title,
       date,
       location,
@@ -41,11 +40,21 @@ const DayC = ({ day, setIsDay, setEvents, events }) => {
     };
     setEvents((prev) => [...prev, newEvent]);
 
-    setTitle("");
-    setDate(day[4]);
-    setLocation("");
-    setDescription("");
     setCreateForm(false);
+  };
+
+  const handleUpdate = (e, item) => {
+    e.preventDefault();
+
+    setEvents((events) =>
+      events.map((event) =>
+        event.id === item.id
+          ? { ...event, title, date, location, description }
+          : event,
+      ),
+    );
+
+    setUpdateForm(false);
   };
 
   return (
@@ -60,6 +69,9 @@ const DayC = ({ day, setIsDay, setEvents, events }) => {
         <div className="d-events-cont">
           {createForm ? (
             <form className="d-form" onSubmit={handleSubmit}>
+              <p className="s-close" onClick={() => setCreateForm(false)}>
+                X
+              </p>
               <input
                 name="title"
                 type="text"
@@ -95,27 +107,93 @@ const DayC = ({ day, setIsDay, setEvents, events }) => {
               </button>
             </form>
           ) : (
-            <button className="b-btn" onClick={() => setCreateForm(true)}>
+            <button
+              className="b-btn"
+              onClick={() => {
+                setCreateForm(true);
+                setUpdateForm(false);
+                setTitle("");
+                setDate(day[4]);
+                setLocation("");
+                setDescription("");
+              }}
+            >
               Add event +
             </button>
           )}
-
           <div className="d-events">
-            {todayEvents.map((event) => (
-              <div className="d-event" key={event.id}>
-                <div className="l-cont">
-                  <h3>{event.title}</h3>
-                  {event.location ? <p>Location: {event.location}</p> : null}
-                  {event.description ? (
-                    <p>Description: {event.description}</p>
-                  ) : null}
+            {todayEvents.map((event) => {
+              return updateForm ? (
+                <div key={event.id}>
+                  <form
+                    action=""
+                    onSubmit={(e) => handleUpdate(e, event)}
+                    className="d-form"
+                  >
+                    <p className="s-close" onClick={() => setUpdateForm(false)}>
+                      X
+                    </p>
+                    <input
+                      name="title"
+                      type="text"
+                      required
+                      placeholder="Title"
+                      value={title}
+                      onChange={handleTitleChange}
+                    />
+                    <input
+                      name="date"
+                      type="date"
+                      required
+                      placeholder="Date"
+                      value={date}
+                      onChange={handleDateChange}
+                    />
+                    <input
+                      name="location"
+                      type="text"
+                      placeholder="Location"
+                      value={location}
+                      onChange={handleLocationChange}
+                    />
+                    <input
+                      name="description"
+                      type="text"
+                      placeholder="Description"
+                      value={description}
+                      onChange={handleDescriptionChange}
+                    />
+                    <button className="b-btn">Update event</button>
+                  </form>
                 </div>
-                <div className="r-cont">
-                  <p>{event.date}</p>
-                  <img src="/pencil.svg" alt="" />
+              ) : (
+                <div className="d-event" key={event.id}>
+                  <div className="l-cont">
+                    <h3>{event.title}</h3>
+                    {event.location ? <p>Location: {event.location}</p> : null}
+                    {event.description ? (
+                      <p>Description: {event.description}</p>
+                    ) : null}
+                  </div>
+                  <div className="r-cont">
+                    <p>{event.date}</p>
+                    <button
+                      className="update-btn"
+                      onClick={() => {
+                        setUpdateForm(event.id);
+                        setCreateForm(false);
+                        setTitle(event.title);
+                        setDate(event.date);
+                        setLocation(event.location);
+                        setDescription(event.description);
+                      }}
+                    >
+                      <img src="/pencil.svg" alt="update" />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </div>
       </div>
