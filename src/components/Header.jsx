1a5023b63a423bf0dce483e09b96f0936f6d54b0
IrynaSobetska <127340294+IrynaSobetska@users.noreply.github.com>
2026-04-22 16:00:56 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "./Components.css";
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/">Home</Link>
-      <Link to="/project">Project</Link>
-      <Link to="/profile">Profile</Link>
+      <Link className="logo" to="/">
+        MintTask
+      </Link>
+      <div className="links">
+        {/* <Link to="/">Home</Link> */}
+        {/* <Link to="/project">Project</Link> */}
+        {/* <Link to="/profile">Profile</Link> */}
+      </div>
     </div>
   );
 };
