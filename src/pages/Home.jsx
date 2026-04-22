d7f966c6cac8c91286a6056e39cae27c0d37d1e8
IrynaSobetska <127340294+IrynaSobetska@users.noreply.github.com>
2026-04-22 16:02:56 +0200

    Revamp Home component with new design and text
    Updated the Home component layout and content to reflect a more minimalistic design and added a new description.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,23 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div className="home-cont">
+      <div className="tiny-p">
+        <svg width="10" height="10">
+          <circle cx="5" cy="5" r="5" fill="#70b99b" />
+        </svg>
+        <p>Minimalistic Task Managment</p>
+      </div>
       <h1>
-        Simple website task manager that should grow in something bigger in
-        future
+        Stay focused. <br /> Get things done.
       </h1>
-      <h2>Enjoi test version</h2>
+      <p className="grey-p">
+        This website is still in development, most of the feature may not work.
+        Enjoy the test version.
+      </p>
 
-      <Link className="b-btn" to="/project">
-        Test available functions
+      <Link className="a-btn" to="/project">
+        Go to Project
       </Link>
     </div>
   );
